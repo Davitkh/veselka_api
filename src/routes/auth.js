7302bdb812ae63153c80f00bc6e58945762a9a06
Edgar <ed.hovhannis1992@gmail.com>
2023-10-23 22:03:28 +0300

    fix server crashing, add cors lib, increase

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,49 +1,48 @@
-import { Router } from "express";
+import { Router } from 'express';
 const router = Router();
-import User from "../model/user.js";
-import bcrypt from "bcryptjs";
+import User from '../model/user.js';
+import bcrypt from 'bcryptjs';
 
-router.post("/signup", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-
-  const user = await User.findOne({ email });
-  if (user) {
-    res.status(400).json({ message: "User is already exists" });
-  } else {
+router.post('/signup', async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
     const hash = await bcrypt.hash(password, 10);
-
-    const newUser = await User({
-      firstName,
-      lastName,
-      email,
-      password: hash,
-    });
-    newUser.save();
-    res.status(201).json({
+    const newUser = new User({ firstName, lastName, email, password: hash });
+    await newUser.save();
+    res.json({
       firstName: newUser.firstName,
       lastName: newUser.lastName,
       email: newUser.email,
-      // password: newUser.,
     });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (user) {
-    const isSame = await bcrypt.compare(password, user.password);
-    if (isSame) {
-      res.status(201).json({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      });
+router.post('/signin', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const isSame = await bcrypt.compare(password, user.password);
+      if (isSame) {
+        res.status(201).json({
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+        });
+      } else {
+        res.status(400).json({ message: 'Password is incorrect' });
+      }
     } else {
-      res.status(400).json({ message: "Password is incorrect" });
+      res.status(400).json({ message: 'User is not found' });
     }
-  } else {
-    res.status(400).json({ message: "User is not found" });
+  } catch (err) {
+    console.log(err);
   }
 });
 
